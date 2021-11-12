c9f457ce49ddfbc162f3ad0caeaa9ddb6ab3bdcb
Marina Soufi <xysamemir@gmail.com>
2021-11-12 11:45:17 +0100

    change-port(5000)

--- src/server/src/user-has-event/dto/update-user-has-event.input.ts
+++ src/server/src/user-has-event/dto/update-user-has-event.input.ts
@@ -2,7 +2,9 @@ import { CreateUserHasEventInput } from './create-user-has-event.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateUserHasEventInput extends PartialType(CreateUserHasEventInput) {
+export class UpdateUserHasEventInput extends PartialType(
+  CreateUserHasEventInput,
+) {
   @Field(() => Int)
   id: number;
 }
