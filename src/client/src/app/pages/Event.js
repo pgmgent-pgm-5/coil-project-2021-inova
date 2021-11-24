2c684f1e381026333ab8cd061c7a2489d1bb4a31
soufieddin <eddin2209@gmail.com>
2021-11-24 18:25:25 +0100

    update

--- src/client/src/app/pages/Event.js
+++ src/client/src/app/pages/Event.js
@@ -28,14 +28,9 @@ query {
   }
 }
 `;
-  
-<<<<<<< HEAD
-const { data} = useQuery(GET_DUES);  
-=======
 const { data} = useQuery(GET_DUES, {
   fetchPolicy: "network-only"
 });  
->>>>>>> release/0.0.2
   const GET_ALL_EXPENSES = gql`
   query {
     getAllExpences(eventId: "${id}"){
